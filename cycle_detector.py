ed01de396391976898180c89f0e660ac41cbed11
sagarhalladakeri-png <halladakerisagar@gmail.com>
2025-12-04 02:26:59 +0530

    added detect_cycles method

--- cycle_detector.py
+++ cycle_detector.py
@@ -21,4 +21,22 @@ class CycleDetector:
         self._visited: Set[str] = set()
         self._recursion_stack: Set[str] = set()
         self._cycle_paths: List[List[str]] = []
+    
+    def detect_cycles(self) -> List[List[str]]:
+        """
+        Detect all cycles in the dependency graph.
+        
+        Returns:
+            List of cycles, where each cycle is a list of file paths
+        """
+        self.cycles = []
+        self._visited = set()
+        self._recursion_stack = set()
+        self._cycle_paths = []
+        
+        for node in self.graph.get_all_nodes():
+            if node not in self._visited:
+                self._dfs(node, [])
+        
+        return self.cycles.copy()
 
